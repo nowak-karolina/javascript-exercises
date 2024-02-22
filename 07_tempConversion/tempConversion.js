00336bf659b3f74fae9301df4cd63cb1b32b7e2c
Karolina Nowak <nowakina15@gmail.com>
2024-02-22 20:12:14 +0100

    07 works

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC=Math.round(((tempF-32)*5/9)*10)/10;
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF=Math.round((tempC*9/5+32)*10)/10;
+  return tempF;
 };
 
+//console.log(convertToCelsius(100));
+//console.log(convertToFahrenheit(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
